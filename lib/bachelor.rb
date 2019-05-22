ce0b7cddc6efd693f058a34eea3e8d38f8d1184d
Xinran Che <gracexinran@gmail.com>
2019-05-22 04:58:38 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,6 @@
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season][0]['name'].split[0]
+  data[season][0][name].split[0]
 end
 
 def get_contestant_name(data, occupation)
