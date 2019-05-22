f7c5a006c52ccfa227cf417cab764aeae73327cd
Xinran Che <gracexinran@gmail.com>
2019-05-22 05:03:12 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,10 +1,11 @@
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season][0][name].split[0]
+  data[season][0]['name'].split[0]
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  
 end
 
 def count_contestants_by_hometown(data, hometown)
